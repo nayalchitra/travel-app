2ab1d05b841f7ffdfd98ed0dbd7996aaa385293d
Chitra Nayal <rekhachitra96@gmail.com>
2024-04-07 18:03:05 +0530

    destination search api

--- controller/hotelController.js
+++ controller/hotelController.js
@@ -4,17 +4,26 @@ const hotelModel = require('../model/hotel.model');
 
 const hotelHandler = async(req,res)=>{
     const hotelCategory = req.query.category;
+    const destination = req.query.destination;
+
     console.log({hotelCategory})
+    console.log({destination});
 
     try {
         let hotelData;
         if(hotelCategory && hotelCategory!=undefined && hotelCategory!='undefined' && hotelCategory !=null)
         {
             hotelData = await hotelModel.hotel.find({category: hotelCategory});
-            console.log("inner ",{hotelData});
+            //console.log("inner ",{hotelData});
+        }
+        else if(destination && destination !=undefined && destination !='undefined' && destination !=null){
+            hotelData = await hotelModel.hotel.find({$or:[{address:destination},{city:destination},{state:destination}]});
+            console.log(hotelData);
         }
         else{
+          
             hotelData = await hotelModel.hotel.find({}) ;
+           // console.log("all:",hotelData)
         }
         hotelData ? res.send(hotelData) : res.send({message:"no data found"});
     } catch (error) {
